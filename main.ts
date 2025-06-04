1a2ca4ed75cf932c7150c6d971b655a9ae89c364
ELIAS EMPRESAS <eliasempresas0@gmail.com>
2025-06-04 19:53:41 -0300

    Add files via upload

---
+++ main.ts
@@ -0,0 +1,10 @@
+//% weight=100 color=#FF0000 icon="star"
+//% block="ELIAS EMPRESAS"
+//% groups='["ELIAS EMPRESAS"]'
+namespace EliasEmpresas {
+    //% block="Rodar ELIAS EMPRESAS"
+    //% group="ELIAS EMPRESAS"
+    export function ELIASEMPRESAS(): void {
+        basic.showString("ELIAS")
+    }
+}
